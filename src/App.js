c13788d49b4ab0926b9eabd23afd3e682831323d
Daniel Osiro <danielosiro@hotmail.com>
2021-08-05 09:44:31 -0300

    React Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // JS
+  // render() {
+  //   const name = "John Doe";
+
+  //   return React.createElement('h1', null, `Hello ${name}`);
+  // }
+
+  // JSX
+  render() {
+    const name = "John Doe";
+
+    return (
+      <div className="App">
+        <h1>Hello {name}</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
